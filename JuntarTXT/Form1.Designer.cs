40ba4f0a2377ce1e8c548a1471cfc5c197c0f8c0
lennon souza <lennon.souza@hotmail.com>
2022-04-04 10:03:38 -0300

    Adicionar arquivos de projeto.

---
+++ JuntarTXT/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace JuntarTXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.ofd_Files = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(274, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Juntar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(12, 9);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(256, 20);
+            this.txt_path.TabIndex = 1;
+            // 
+            // ofd_Files
+            // 
+            this.ofd_Files.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 41);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Powered LimitsGOD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.OpenFileDialog ofd_Files;
+    }
+}
+
---
+++ JuntarTXT/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuntarTXT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> files = Directory.GetFiles(this.txt_path.Text, "*.*", SearchOption.AllDirectories).Where(f => f.EndsWith(".txt"));
+            bool flag = files.Count() != 0;
+            if (flag)
+            {
+                FileStream fileStream = File.Open(@"file.txt", FileMode.OpenOrCreate);
+                fileStream.Close();
+                List<string> list = new List<string>();
+                foreach (string path2 in files)
+                {
+                    list.AddRange(File.ReadAllLines(path2));
+                }
+                File.WriteAllLines(@"file.txt", list.ToArray());
+                MessageBox.Show($"Save file in '{AppDomain.CurrentDomain.BaseDirectory}file.txt'!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
